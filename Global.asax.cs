09eca6ceeedb0e0d707b7fe6223b9da807f36485
MrShaggie <46590461+MrShaggie@users.noreply.github.com>
2020-06-21 22:53:12 +0530

    Add other required files

---
+++ Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using ProjectScheduling.DAL;
+using ProjectScheduling.Infrastructure;
+
+namespace ProjectScheduling
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RegisterAutofacDependencies();
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void RegisterAutofacDependencies()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            builder.RegisterType<DoctorDAL>().As<IDoctorDAL>();
+            builder.RegisterType<AppointmentDAL>().As<IAppointmentDAL>();
+            builder.RegisterType<SchedulingDbContext>();
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectScheduling.Startup))]
+namespace ProjectScheduling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
